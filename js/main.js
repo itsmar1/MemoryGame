cb8ac3c24f9d17f26fc8f9282ec21e6e07f051c4
Marouane <d144013152@gmail.com>
2020-11-06 22:17:55 +0100

    Create Splash Screen

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.querySelector('.control-buttons span').onclick = function(){
+
+    let yourName = prompt("What's Your Name?");
+
+    if ( yourName == null || yourName == "" ) {
+        document.querySelector('.name span').innerHTML = 'Unknown';
+    }
+    else {
+        document.querySelector('.name span').innerHTML = yourName;
+    }
+
+    document.querySelector('.control-buttons').remove();
+}
+
