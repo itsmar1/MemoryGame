fcb609d6cbd1e218e474f3624dcf4d8966ded3ac
Marouane <d144013152@gmail.com>
2020-11-09 18:21:44 +0100

    Create The Shuffle Function

--- js/main.js
+++ js/main.js
@@ -20,14 +20,46 @@ var blocks = Array.from(blocksContainer.children);
 
 var orderRange = [...Array(blocks.length).keys()];
 // Another method to create range of keys
-var orderRangee = Array.from(Array(blocks.length).keys());
-
+//var orderRangee = Array.from(Array(blocks.length).keys());
+console.log(orderRange);
+shuffle(orderRange);
+console.log(orderRange);
 
 // Add order CSS property the game
 blocks.forEach( (block, index) => {
     
-    block.style.order = index;
+    block.style.order = orderRange[index];
 });
+
+
+// Create Shuffle function 
+
+function shuffle(array) {
+
+    // get the current index
+    let current = array.length,
+        random,
+        temp;
+
+
+    while ( current > 0 ) {
+
+        // get a random number
+        random = Math.floor(Math.random() * current );
+
+        current--;
+        
+        temp = array[current];
+        array[current] = array[random];
+        array[random] = temp;
+
+    }
+
+    return array;
+    
+}
+
+
 
 
 
